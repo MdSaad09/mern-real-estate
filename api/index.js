c5f8dcf567595401d82beb604d9d3862b4109e5e
MdSaad09 <mhdsaadkhan098@gmail.com>
2025-04-09 22:41:36 +0530

    add dotenv and mongoose dependencies, connect to database;

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import  express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mern-estate').then(() => {
+  console.log('MongoDB connected successfully');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
